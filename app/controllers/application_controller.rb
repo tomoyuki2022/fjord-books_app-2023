42d49900dd1aa4793d6ecde92f8d26e31a96a953
tomoyuki <at01110833@gmail.com>
2024-01-04 16:35:44 +0900

    ログイン状態でアカウント登録画面にアクセスしたときのリダイレクト先の変更の追加。郵便番号、住所、自己紹介の値を受け入れる処理の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_resource)
+    user_path(id: current_user.id)
+  end
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[postal_code address introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address introduction])
+  end
 end
