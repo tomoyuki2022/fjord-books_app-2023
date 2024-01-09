3a5765c0dde932aac5dcf0a9fcc07249d5809c43
tomoyuki <at01110833@gmail.com>
2024-01-09 14:35:39 +0900

    after_sign_in_path_forの削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,10 +25,6 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  def after_sign_in_path_for(_resource)
-    books_path
-  end
-
   def after_sign_out_path_for(_resource)
     new_user_session_path
   end
