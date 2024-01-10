676e28fcc66f4f0ac5ba8adcc615ba1519158726
tomoyuki <at01110833@gmail.com>
2024-01-10 12:28:15 +0900

    after_sign_out_path_forの削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,8 +24,4 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-
-  def after_sign_out_path_for(_resource)
-    new_user_session_path
-  end
 end
