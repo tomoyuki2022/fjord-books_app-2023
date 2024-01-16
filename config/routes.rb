17661baba5047d1bc0861e8f00304dd4623991d2
tomoyuki <at01110833@gmail.com>
2024-01-16 11:59:24 +0900

    SessionsControllerを削除したための変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
   }
 
   devise_scope :user do
-    get '/users/sign_out' => 'users/sessions#destroy'
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   resources :books
