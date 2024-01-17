81c3b270b09e8e7a827d3810573b0dff4fea8187
tomoyuki <at01110833@gmail.com>
2024-01-17 15:29:58 +0900

    N+1問題を解決させるための変更とiconをパラメータで許可する処理の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,16 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.with_attached_icon.order(:id).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:icon)
   end
 end
