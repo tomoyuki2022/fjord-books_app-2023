2fbada79df9efa0d43f45499ad30fc8d4d556529
tomoyuki <at01110833@gmail.com>
2024-02-23 16:42:12 +0900

    mentionの作成、更新の実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,20 +19,25 @@ class ReportsController < ApplicationController
   def edit; end
 
   def create
-    @report = current_user.reports.new(report_params)
-
-    if @report.save
-      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
-    else
-      render :new, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      @report = current_user.reports.new(report_params)
+      if @report.save
+        create_mention
+        redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
   def update
-    if @report.update(report_params)
-      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      if @report.update(report_params)
+        update_mention
+        redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
   end
 
@@ -50,5 +55,22 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:title, :content)
+  end
+
+  def create_mention
+    id = @report.id
+    extracted_ids = @report.extract_ids.uniq
+    extracted_ids.each do |extracted_id|
+      Mention.create!(mentioning_report_id: id, mentioned_report_id: extracted_id)
+    end
+  end
+
+  def update_mention
+    id = @report.id
+    extracted_ids = @report.extract_ids.uniq
+    Mention.where(mentioning_report_id: id).destroy_all
+    extracted_ids.each do |extracted_id|
+      Mention.create!(mentioning_report_id: id, mentioned_report_id: extracted_id)
+    end
   end
 end
