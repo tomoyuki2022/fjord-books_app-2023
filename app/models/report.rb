6235ed3a358e9b0722020deea3187316f430b728
tomoyuki <at01110833@gmail.com>
2024-02-23 16:43:21 +0900

    多対多の関連付けの設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,11 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :mentioning, class_name: 'Mention', foreign_key: 'mentioning_report_id', dependent: :destroy, inverse_of: :mentioning_report
+  has_many :mentioning_reports, through: :mentioning, source: :mentioned_report
+
+  has_many :mentioned, class_name: 'Mention', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioned_reports, through: :mentioned, source: :mentioning_report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -13,5 +18,9 @@ class Report < ApplicationRecord
 
   def created_on
     created_at.to_date
+  end
+
+  def extract_ids
+    content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
   end
 end
