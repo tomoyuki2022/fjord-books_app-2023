d1944cc4d4bc875585840fc662712bfc9b07c0ad
tomoyuki <at01110833@gmail.com>
2024-03-08 11:46:56 +0900

    validationの追加

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :mentioning_report, class_name: 'Report'
   belongs_to :mentioned_report, class_name: 'Report'
+
+  validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
 end
